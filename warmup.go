6d1c8da1c43ef02ee1b62af7b4e88abd8f5e1ff8
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-06-17 16:11:08 -0700

    adding struct

---
+++ warmup.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+)
+
+type House struct {
+  numberOfRooms int
+  city string
+  address string
+  price int
+}
+
+func main() {
+  i := 1
+  max := 2
+}
